b595e12a71f2d1f141e54cdac3be1c0606bc4e9d
silviapoliveira <correiodasilviaoliveira@gmail.com>
2024-07-22 16:24:12 +0100

    InitialDb

---
+++ Sales_NET8.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Sales_NET8.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sales_NET8.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
